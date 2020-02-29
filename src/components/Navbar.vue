413ed6c7af772ef66862450ca3e1a9e440900002
z3by <zoaby.am@gmail.com>
2020-03-01 00:29:10 +0200

    add simple Navbar ✍️

---
+++ src/components/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav>
+    <ul class="flex">
+      <li class="mr-6">
+        <router-link to="/" class="font-bold uppercase">Friday Souq</router-link>
+      </li>
+      <li class="mr-6">
+        <router-link
+          class="text-blue-500 hover:text-blue-800"
+          to="/"
+        >Home</router-link>
+      </li>
+      <li class="mr-6">
+        <router-link
+          class="text-blue-500 hover:text-blue-800"
+          to="/"
+        >About</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home p-10">
+    <Navbar />
+    <header class=" py-10 text-center flex justify-center">
+      <div class="shadow p-10 rounded">
+      <h1 class="text-4xl">Hi!</h1>
+      <h2 class="text-2xl text-gray-600">Welcome to Friday Souq</h2>
+      </div>
+    </header>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Navbar from '@/components/Navbar.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Navbar
   }
 }
 </script>
